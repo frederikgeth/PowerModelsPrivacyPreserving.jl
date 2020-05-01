791bc564aff213b33939e66debebcea275998076
afeutrill <andrew.feutrill@gmail.com>
2020-05-01 17:59:12 +0930

    Add shunts (#21)
    * Adding shunt admmittance privacy to optimisation
    
    * Altered one line to not perturb g_shunt
    
    * Fixed up the path I changed

--- src/core/data.jl
+++ src/core/data.jl
@@ -21,25 +21,36 @@ function create_impedance_perturbation(data_input, α, ϵ, λ)
     distribution = Distributions.Laplace(0, 3 * α / ϵ)
     sum_g = 0
     sum_b = 0
+    sum_g_shunt = 0
+    sum_b_shunt = 0
     for (l, branch) in data["branch"]
         noise = Random.rand(distribution, 1)[1]
         z = branch["br_r"] + im*branch["br_x"]
         y = 1/z
         g = real(y)
         b = imag(y)
+        g_shunt = branch["g_to"]
+        b_shunt = branch["b_to"]
         # println("g is: ", g)
         # println("b is: ", b)
 
         r = b / g # Algorithm 1 and eq. (15)
         pert_g = g + noise # noisy conductances
         pert_b = r * pert_g # get noisy susceptances
+        r_shunt = g_shunt/b_shunt
+        pert_b_shunt = b_shunt + noise_shunt
+        pert_g_shunt = g_shunt # Removed the perturbation of g_shunt
 
         branch["g_obj"] = pert_g
         branch["b_obj"] = pert_b
+        branch["g_shunt_obj"] = pert_g_shunt
+        branch["b_shunt_obj"] = pert_b_shunt
         # println("perturbed g is: ", pert_g)
         # println("perturbed b is: ", pert_b)
         sum_g += g
         sum_b += b
+        sum_g_shunt += g_shunt
+        sum_b_shunt += b_shunt
     end
 
     # Apply the noisy mean limit values for s4 and s5
@@ -48,6 +59,8 @@ function create_impedance_perturbation(data_input, α, ϵ, λ)
 
     μ_g = (1 / n) * sum_g + Random.rand(lap_μ, 1)[1] # Eq 16
     μ_b = (1 / n) * sum_b + Random.rand(lap_μ, 1)[1] # Eq 17
+    μ_g_shunt = (1 / n_branches) * sum_g_shunt + Random.rand(distribution, 1)[1]
+    μ_b_shunt = (1 / n_branches) * sum_b_shunt + Random.rand(distribution, 1)[1]
 
     # Add noisy mean limit values to our data dictionary
     # Note that we use min and max to handle negative parameters
@@ -55,6 +68,10 @@ function create_impedance_perturbation(data_input, α, ϵ, λ)
     data["g_ub"] = max(μ_g / λ, μ_g * λ)
     data["b_lb"] = min(μ_b / λ, μ_b * λ)
     data["b_ub"] = max(μ_b / λ, μ_b * λ)
+    data["g_lb_shunt"] = min(μ_g_shunt / λ, μ_g_shunt * λ)
+    data["g_ub_shunt"] = max(μ_g_shunt / λ, μ_g_shunt * λ)
+    data["b_lb_shunt"] = min(μ_b_shunt / λ, μ_b_shunt * λ)
+    data["b_ub_shunt"] = max(μ_b_shunt / λ, μ_b_shunt * λ)
 
     return data
 end
--- src/core/variable.jl
+++ src/core/variable.jl
@@ -8,16 +8,30 @@ function variable_admittance(pm::PMs.AbstractPowerModel; nw::Int=pm.cnw, bounded
         [ l in PMs.ids(pm, nw, :branch)], base_name="$(nw)_b",
         # start = PMs.comp_start_value(PMs.ref(pm, nw, :branch, l), "y_start")
     )
+    g_shunt = PMs.var(pm, nw)[:g_shunt] = JuMP.@variable(pm.model,
+        [ l in PMs.ids(pm, nw, :branch)], base_name="$(nw)_g_shunt",
+        # start = PMs.comp_start_value(PMs.ref(pm, nw, :branch, l), "y_start")
+    )
+    b_shunt = PMs.var(pm, nw)[:b_shunt] = JuMP.@variable(pm.model,
+        [ l in PMs.ids(pm, nw, :branch)], base_name="$(nw)_b_shunt",
+        # start = PMs.comp_start_value(PMs.ref(pm, nw, :branch, l), "y_start")
+    )
     # Limit the upper and lower bound of each g,b for model constraint s4 and s5
     for (l,branch) in PMs.ref(pm, nw, :branch)
         JuMP.set_lower_bound(g[l], PMs.ref(pm, nw, :g_lb))
         JuMP.set_upper_bound(g[l], PMs.ref(pm, nw, :g_ub))
         JuMP.set_lower_bound(b[l], PMs.ref(pm, nw, :b_lb))
         JuMP.set_upper_bound(b[l],PMs.ref(pm, nw, :b_ub))
+        JuMP.set_lower_bound(g_shunt[l], PMs.ref(pm, nw, :g_shunt_lb))
+        JuMP.set_upper_bound(g_shunt[l], PMs.ref(pm, nw, :g_shunt_ub))
+        JuMP.set_lower_bound(b_shunt[l], PMs.ref(pm, nw, :b_shunt_lb))
+        JuMP.set_upper_bound(b_shunt[l],PMs.ref(pm, nw, :b_shunt_ub))
     end
 
     report && PMs.sol_component_value(pm, nw, :branch, :g, PMs.ids(pm, nw, :branch), g)
     report && PMs.sol_component_value(pm, nw, :branch, :b, PMs.ids(pm, nw, :branch), b)
+    report && PMs.sol_component_value(pm, nw, :branch, :g_shunt, PMs.ids(pm, nw, :branch), g_shunt)
+    report && PMs.sol_component_value(pm, nw, :branch, :b_shunt, PMs.ids(pm, nw, :branch), b_shunt)
 end
 
 function variable_fuel_cost(pm::PMs.AbstractPowerModel, report::Bool=true)
