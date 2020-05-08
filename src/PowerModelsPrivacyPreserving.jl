d15005258fe0022f33f195efaa757fe397262fec
Geth, Frederik (Energy, Newcastle) <Frederik.Geth@csiro.au>
2020-05-08 13:48:56 +1000

    Workaround short-circuiting export_extra_data

--- src/PowerModelsPrivacyPreserving.jl
+++ src/PowerModelsPrivacyPreserving.jl
@@ -6,6 +6,11 @@ import Distributions
 
 const PMs = PowerModels
 
+"Work around bug in PowerModels by short-circuiting export_extra_data"
+function PMs._export_extra_data(io::IO, data::Dict{String,<:Any}, component, excluded_fields=Set(["index", "source_id"]); postfix="")
+    #do nothing
+end
+
 include("core/data.jl")
 include("core/variable.jl")
 include("core/constraint.jl")
@@ -14,5 +19,7 @@ include("core/objective.jl")
 
 include("prob/opf.jl")
 include("prob/opf_impedance.jl")
+
+
 
 end # module
